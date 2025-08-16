6ce923130c37dea2cf9f811e478e91e3dee44c0b
xxanqw <potik217@gmail.com>
2025-08-16 04:58:34 +0300

    1.21.5

--- src/main/java/pp/ua/xxanqw/litematica_wand/LitematicaWand.java
+++ src/main/java/pp/ua/xxanqw/litematica_wand/LitematicaWand.java
@@ -1,6 +1,9 @@
 package pp.ua.xxanqw.litematica_wand;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pp.ua.xxanqw.litematica_wand.item.ModItems;
@@ -15,5 +18,12 @@ public class LitematicaWand implements ModInitializer {
         LOGGER.info("Initializing Litematica Wand");
         ModItems.registerModItems();
         ModItemsGroup.registerItemGroups();
+        
+        // Register tooltip for the wand item
+        ItemTooltipCallback.EVENT.register((itemStack, tooltipContext, tooltipType, list) -> {
+            if (itemStack.isOf(ModItems.WAND)) {
+                list.add(Text.translatable("item.litematica_wand.wand.tooltip").formatted(Formatting.BOLD));
+            }
+        });
     }
 }
--- src/main/java/pp/ua/xxanqw/litematica_wand/item/WandItem.java
+++ src/main/java/pp/ua/xxanqw/litematica_wand/item/WandItem.java
@@ -1,20 +1,9 @@
 package pp.ua.xxanqw.litematica_wand.item;
 
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.tooltip.TooltipType;
-import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
-
-import java.util.List;
 
 public class WandItem extends Item {
     public WandItem(Settings settings){
         super(settings);
-    }
-
-    @Override
-    public void appendTooltip(ItemStack itemStack, TooltipContext context, List<Text> tooltip, TooltipType type) {
-        tooltip.add(Text.translatable("item.litematica_wand.wand.tooltip").formatted(Formatting.BOLD));
     }
 }
